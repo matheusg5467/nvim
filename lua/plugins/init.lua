44b57fb31737e39b1816bfc458062c96b50de763
matheusg <matheusgvrodrigues@proton.me>
2025-12-14 14:37:29 -0300

    Fixed a bug with nvim-treesitter, now the branch used is explicitly the master branch

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -3,7 +3,7 @@ vim.pack.add({
     {src = "https://github.com/neovim/nvim-lspconfig"},
     {src = "https://github.com/mason-org/mason.nvim"},
     {src = "https://github.com/mason-org/mason-lspconfig.nvim"},
-    {src = "https://github.com/nvim-treesitter/nvim-treesitter"},
+    {src = "https://github.com/nvim-treesitter/nvim-treesitter", version = 'master'},
     {src = "https://github.com/nvim-lua/plenary.nvim"},
     {src = "https://github.com/nvim-telescope/telescope.nvim"},
     {src = "https://github.com/nvim-telescope/telescope-ui-select.nvim"},
